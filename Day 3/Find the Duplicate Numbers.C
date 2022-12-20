5ab0ab0551e90c7383169539d0f587ff6450d2f1
srivani2410 <114388689+srivani2410@users.noreply.github.com>
2022-12-20 20:50:22 +0530

    Create Find the Duplicate Numbers.C

---
+++ Day 3/Find the Duplicate Numbers.C
@@ -0,0 +1,16 @@
+int findDuplicate(int* num, int numSize){
+        int slow = num[0];
+        int fast = num[0];
+        do{
+            slow=num[slow];
+            fast=num[num[fast]];
+        }while(slow!=fast);
+        fast=num[0];
+        while(slow!=fast){
+            slow=num[slow];
+            fast=num[fast];
+
+        }
+    
+    return slow;
+}
