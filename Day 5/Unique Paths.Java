bb8311558eaef017f22528b06689e7b022fe9a22
srivani2410 <114388689+srivani2410@users.noreply.github.com>
2022-12-20 20:54:15 +0530

    Create Unique Paths.Java

---
+++ Day 5/Unique Paths.Java
@@ -0,0 +1,12 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int N=n+m-2;
+        int r=m-1;
+        double result=1;
+        for(int i=1; i<=r; i++){
+            result=result*(N-r+i)/i;
+        }
+        return (int)result;
+        
+    }
+}
